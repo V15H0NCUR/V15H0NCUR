003e5d57faf4945a296f8cbb5c6670c8b20a8ca7
V15H0NCUR <90255581+V15H0NCUR@users.noreply.github.com>
2021-09-07 14:36:37 +0200

    Add files via upload

---
+++ zad3.php
@@ -0,0 +1,21 @@
+<?php
+function silnia($n){
+    if($n ==1){
+        return 1;
+
+    }
+else{
+    return $n* silnia($n-1);
+}
+}
+if(isset($_POST['Show'])){
+    $liczba = $_POST['liczba'];
+    echo(silnia($liczba));
+}
+
+
+?>
+<form action="" method= "POST">
+<input type="text" name="liczba">
+<input type="submit" name="Show" value='show'>
+</form>
